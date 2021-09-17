28efbd8123c739c2250c5a51f38efea37aae23f1
kapil551 <59477738+kapil551@users.noreply.github.com>
2021-09-18 02:54:27 +0530

    Header - building the left section/side of the header

--- airbnb-clone/components/Header.js
+++ airbnb-clone/components/Header.js
@@ -1,15 +1,106 @@
 // Header component
 
+// import the next.js Image tag
+import Image from 'next/image';
+
 // react functional export component
 
 function Header() {
     return (
 
-        <header>
-            <h2>I am the header</h2>
+            /*
+                Tailwind CSS:
 
-            {/* Left Section of the Header */}
-            <div>
+                class ==> Properties
+
+                sticky ==> 
+                        position: sticky; 
+                [stick to the top of the page]
+                
+                top-0 ==> 
+                        top: 0px;
+                
+                z-50 ==> 
+                        z-index: 50; 
+                [z-index tells which element should come in front ==> the element having the higher z-index will come in front]
+                
+                grid ==> 
+                        display: grid; 
+                [I want my header to be a container with display grid]
+                
+                grid-cols-3 ==> 
+                        grid-template-columns: repeat(3, minmax(0, 1fr));
+                [I want the header to be a grid, containing 3-columns]
+
+                bg-white ==> 
+                        --tw-bg-opacity: 1;
+                        background-color: rgba(255, 255, 255, var(--tw-bg-opacity));
+
+                shadow-md ==> 
+                        --tw-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);
+                        box-shadow: var(--tw-ring-offset-shadow, 0 0 #0000), var(--tw-ring-shadow, 0 0 #0000), var(--tw-shadow);
+                [This is will give a nice shadow underneath the header element]
+                
+                py-5 ==>
+                        padding-top: 1.25rem;
+                        padding-bottom: 1.25rem;
+                
+                px-5 ==> 
+                        padding-left: 1.25rem;
+                        padding-right: 1.25rem;
+                
+                md:px-10 ==> [Adding media queries]
+                        @media (min-width: 768px) {
+                            .md\:px-10 {
+                                padding-left: 2.5rem;
+                                padding-right: 2.5rem;
+                            }
+                        }
+                [only apply px-10 when the width is >= 768px]
+                [media queries for adding responsive designs are known as "Breakpoints" in tailwind CSS]
+                
+            */
+        <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md py-5 px-5
+                           md:px-10">
+            
+            {/* Left Section of the Header -> Airbnb Logo */}
+            
+                {/* Tailwind CSS:
+
+                    class ==> Properties
+                    
+                    relative ==> 
+                            postion: relative;
+                    
+                    h-10 ==> 
+                            height: 2.5rem;
+                    
+                    flex ==> 
+                            display: flex;
+                    
+                    items-center ==> 
+                            align-items: center;
+                    
+                    cursor-pointer ==> 
+                            cursor: pointer;
+                    
+                    my-auto ==> 
+                            margin-top: auto; 
+                            margin-bottom: auto;
+
+                */}
+            <div className="relative h-10 flex items-center cursor-pointer my-auto">
+                
+                {/* Image
+                        <Image / > ==> this is the next.js image tag
+                        It optimises the image, and is way better than the html img tag(<img src="" alt="" />).
+                */}
+                <Image 
+                    src="https://links.papareact.com/qd3"
+                    layout="fill"
+                    objectFit="contain"
+                    objectPosition="left"
+                />
 
             </div>
 
---
+++ airbnb-clone/next.config.js
@@ -0,0 +1,7 @@
+// https://nextjs.org/docs/messages/next-image-unconfigured-host
+
+module.exports = {
+    images: {
+      domains: ['links.papareact.com'],
+    },
+  }
