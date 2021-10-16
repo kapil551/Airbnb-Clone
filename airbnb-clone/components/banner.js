a56e2fb53c8febb6d716751a3a67988ec03f4a6b
kapil551 <59477738+kapil551@users.noreply.github.com>
2021-10-17 04:34:38 +0530

    Banner - added Image to the banner section

--- airbnb-clone/components/banner.js
+++ airbnb-clone/components/banner.js
@@ -1,12 +1,20 @@
 // Banner Component
 
+// import the next.js Image tag
+import Image from 'next/image';
+
 // _rfce ==> react functional export component
 
 function Banner() {
     return (
+
         <div>
 
-            <h1> This is the banner component</h1>
+            <Image 
+                src="https://links.papareact.com/0fm" 
+                layout="fill"
+                objectFit="cover"
+            />
 
         </div>
     )
