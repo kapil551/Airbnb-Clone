661471777400ac99bc34e32bf44f30deb521613a
kapil551 <59477738+kapil551@users.noreply.github.com>
2021-10-18 00:51:19 +0530

    SmallCard - Added some styling to the small card component

--- airbnb-clone/components/smallCard.js
+++ airbnb-clone/components/smallCard.js
@@ -8,7 +8,23 @@ import Image from 'next/image';
         // using destructing to access props.img, props.location, props.distance
 function SmallCard( { img, location, distance }) {
     return (
-        <div>
+
+        /*
+            Styling Using Tailwind CSS:
+
+            space-x-4 ==> iss parent div ke all the children elements mein x-direction ki taraf 4 ki margin/space de do 
+                          to the right side.
+            
+            Adding a hover effect to every small card:
+                1. I want every small card to have a gray backgound when i hover over it ==> hover:bg-gray-100
+                2. and also i want every small card to scale up to 105 when i hove over it ==> hover:scale-105
+                3. add some smooth transition effect with duration of 200 milli seconds ==> transition transform duration-200 
+                4. add some easing transition effect ==> ease-out
+
+        */
+        <div className="border-2 border-red-900 flex items-center m-2 mt-5 space-x-4 rounded-xl cursor-pointer 
+                hover:bg-gray-100 hover:scale-105 transition transform duration-200 ease-out">
+
             {/* <h2> {img} </h2>
             <h2> {location} </h2>
             <h2> {distance} </h2> */}
@@ -21,7 +37,7 @@ function SmallCard( { img, location, distance }) {
             {/* right side of a small card*/}
             <div>
                 <h2> {location} </h2>
-                <h3> {distance} </h3>
+                <h3 className="text-gray-500"> {distance} </h3>
             </div>
 
         </div>
