aab0f50b6b559370839b8ae085d53e543265c2d9
kapil551 <59477738+kapil551@users.noreply.github.com>
2021-10-18 06:06:36 +0530

    Footer - prepare a grid of footer div's using CSS grid (Very Important)

--- airbnb-clone/components/footer.js
+++ airbnb-clone/components/footer.js
@@ -5,7 +5,25 @@
 function Footer() {
     return (
 
-        <div className="border-2 border-gray-800">
+        /* 
+            prepare a grid of footer div's using CSS grid property (Very Important) 
+
+                grid ==> display: grid;
+
+                1. mobile view ==> 
+                    I want the grid to have only one column for mobile ==> grid-cols-1
+                
+                2. medium window (width >= 768px ) ==> 
+                    I want the grid to have only four columns for small window ==>  md:grid-cols-4
+
+                gap-y-10 ==> row-gap: 2.5rem;
+                    row-gap (grid-row-gap):
+                        https://developer.mozilla.org/en-US/docs/Web/CSS/row-gap
+                    grid ki row mein gap kar do 10 ka.
+                             
+        */
+        <div className="border-2 border-gray-800 grid grid-cols-1 md:grid-cols-4 
+                gap-y-10 px-32 py-14 bg-gray-100 text-gray-600">
 
             {/* ABOUT div */}
             {/* 
