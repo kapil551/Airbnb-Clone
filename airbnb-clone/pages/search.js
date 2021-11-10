d439fd02d572231c7970e1e3afbca05995c650f4
kapil551 <59477738+kapil551@users.noreply.github.com>
2021-11-11 03:20:21 +0530

    Search Page - added the search page component under the folder pages
    now go to http://localhost:3000/search

---
+++ airbnb-clone/pages/search.js
@@ -0,0 +1,14 @@
+// Search Page Component
+// _rfce ==> react functional export component
+
+function Search() {
+    return (
+        <div>
+            <h1> I am the Search Page </h1>
+        </div>
+    )
+}
+
+export default Search;
+
+// now go to http://localhost:3000/search
