8fcc835aa0d7c358ee7a86c1ddbeb68a9f885e3c
kapil551 <59477738+kapil551@users.noreply.github.com>
2021-11-11 03:27:33 +0530

    Search Page - added the header and footer to the search page component

--- airbnb-clone/pages/search.js
+++ airbnb-clone/pages/search.js
@@ -1,10 +1,22 @@
 // Search Page Component
 // _rfce ==> react functional export component
 
+// import the Header component
+import Header from "../components/Header";
+
+// import the Footer component
+import Footer from '../components/footer';
+
 function Search() {
     return (
-        <div>
-            <h1> I am the Search Page </h1>
+        <div className="h-screen">
+
+            {/* Header ==> Use the Header component */}
+           <Header />
+
+           {/* Footer */}
+           <Footer />
+
         </div>
     )
 }
